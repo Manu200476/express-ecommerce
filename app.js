cfebf94b41f5b3d1a51a2f8f1e7f1808708ee507
Manuel Martin <manuzombiesbf1@gmailc.com>
2021-08-10 07:21:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const errorController = require('./controllers/error');
+const sequelize = require('./util/sequelize')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+sequelize
+    .sync()
+    .then(res => console.log(res))
+    .catch(e => console.log(e))
+
+app.listen(3000);
